c66cb73f781bbf54802e1923710b2c11e45aa30a
David Montiel <dmontiel@umich.edu>
2024-10-05 15:29:21 -0400

    Added phase field solver mechod before solve_cp in run.cc. Modified parameters to use same pf and cpfe mesh.

--- src/materialModels/crystalPlasticity/run.cc
+++ src/materialModels/crystalPlasticity/run.cc
@@ -1,39 +1,41 @@
-//run method for multiPhysicsBVP class
+// run method for multiPhysicsBVP class
 #include "../../../include/crystalPlasticity.h"
 #include <sys/stat.h>
 
-//this source file is temporarily treated as a header file (hence
-//#ifndef's) till library packaging scheme is finalized
+// this source file is temporarily treated as a header file (hence
+// #ifndef's) till library packaging scheme is finalized
 
-template <int dim>
-void crystalPlasticity<dim>::run(){
+template <int dim> void crystalPlasticity<dim>::run() {
 
-  const int dir_err = mkdir(this->userInputs_cp.outputDirectory.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-  //initialization
-  #if ((DEAL_II_VERSION_MAJOR < 9)||((DEAL_II_VERSION_MINOR < 3)&&(DEAL_II_VERSION_MAJOR==9)))
+  const int dir_err = mkdir(this->userInputs_cp.outputDirectory.c_str(),
+                            S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+// initialization
+#if ((DEAL_II_VERSION_MAJOR < 9) ||                                            \
+     ((DEAL_II_VERSION_MINOR < 3) && (DEAL_II_VERSION_MAJOR == 9)))
   this->computing_timer_cp.enter_section("mesh and initialization");
-  #else
+#else
   this->computing_timer_cp.enter_subsection("mesh and initialization");
-  #endif
+#endif
 
-  //READING AND INITIALIZATION (PRISMS-Plasticity)
-  //read mesh;
+  // READING AND INITIALIZATION (PRISMS-Plasticity)
+  // read mesh;
   this->mesh();
-  //initialize FE objects and global data structures
+  // initialize FE objects and global data structures
   this->init_cp();
   this->initProjection();
-  #if ((DEAL_II_VERSION_MAJOR < 9)||((DEAL_II_VERSION_MINOR < 3)&&(DEAL_II_VERSION_MAJOR==9)))
+#if ((DEAL_II_VERSION_MAJOR < 9) ||                                            \
+     ((DEAL_II_VERSION_MINOR < 3) && (DEAL_II_VERSION_MAJOR == 9)))
   this->computing_timer_cp.exit_section("mesh and initialization");
-  #else
+#else
   this->computing_timer_cp.leave_subsection("mesh and initialization");
-  #endif
-  //READING AND INITIALIZATION (PRISMS-PF)
+#endif
+  // READING AND INITIALIZATION (PRISMS-PF)
   pf_object.buildFields();
   pf_object.init_pf();
+  pf_object.solve_pf();
 
   this->solve_cp();
 
-  //pf_object.solve_pf();
-
+  // pf_object.solve_pf();
 }
 #include "../../../include/crystalPlasticity_template_instantiations.h"
